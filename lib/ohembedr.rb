1f7fbff90db4e5fb9f60c2c1241c090af6351215
Ben McRedmond <ben@benmcredmond.com>
2010-06-28 12:51:56 +0100

    Typo in ohembedr

--- lib/ohembedr.rb
+++ lib/ohembedr.rb
@@ -147,7 +147,7 @@ module OhEmbedr
       response = nil
       
       Net::HTTP::Proxy(proxy[0], proxy[1]).start(uri.host) do |h|   
-        response = h.get(uri.request_urigem)
+        response = h.get(uri.request_uri)
       end
       
       raise response.code if response.code != "200"
